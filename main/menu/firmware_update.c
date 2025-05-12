07e22b6a703ce6ed2b1d728f7c260d6ffa196a4b
Renze Nicolai <renze@rnplus.nl>
2025-05-12 23:31:04 +0200

    Change URL for OTA server and add options for updating to staging and experimental builds

--- main/menu/firmware_update.c
+++ main/menu/firmware_update.c
@@ -19,10 +19,10 @@
 
 static void firmware_update_callback(const char* status_text) {
     printf("OTA status changed: %s\r\n", status_text);
-    pax_buf_t* fb = display_get_buffer();
-    pax_draw_rect(fb, 0xFFEEEAEE, 0, 65, fb->width, 32);
-    pax_draw_text(fb, 0xFF340132, &chakrapetchmedium, 16, 20, 70, status_text);
-    display_blit_buffer(fb);
+    pax_buf_t* buffer = display_get_buffer();
+    pax_draw_rect(buffer, 0xFFEEEAEE, 0, 85, buffer->width, 32);
+    pax_draw_text(buffer, 0xFF340132, &chakrapetchmedium, 16, 20, 90, status_text);
+    display_blit_buffer(buffer);
 }
 
 void menu_firmware_update(pax_buf_t* buffer, gui_theme_t* theme) {
@@ -31,6 +31,24 @@ void menu_firmware_update(pax_buf_t* buffer, gui_theme_t* theme) {
     gui_render_header_adv(buffer, theme, ((gui_header_field_t[]){{get_icon(ICON_SYSTEM_UPDATE), "Firmware update"}}), 1,
                           NULL, 0);
     gui_render_footer_adv(buffer, theme, NULL, 0, NULL, 0);
-    display_blit_buffer(buffer);
-    ota_update("https://ota.tanmatsu.cloud/latest.bin", firmware_update_callback);
+
+    bool staging = false;
+    bsp_input_read_navigation_key(BSP_INPUT_NAVIGATION_KEY_F2, &staging);
+
+    bool experimental = false;
+    bsp_input_read_navigation_key(BSP_INPUT_NAVIGATION_KEY_F3, &experimental);
+
+    if (experimental) {
+        pax_draw_text(buffer, 0xFF340132, &chakrapetchmedium, 16, 20, 70, "Update target: experimental");
+        display_blit_buffer(buffer);
+        ota_update("https://selfsigned.ota.tanmatsu.cloud/experimental.bin", firmware_update_callback);
+    } else if (staging) {
+        pax_draw_text(buffer, 0xFF340132, &chakrapetchmedium, 16, 20, 70, "Update target: staging");
+        display_blit_buffer(buffer);
+        ota_update("https://selfsigned.ota.tanmatsu.cloud/staging.bin", firmware_update_callback);
+    } else {
+        pax_draw_text(buffer, 0xFF340132, &chakrapetchmedium, 16, 20, 70, "Update target: stable");
+        display_blit_buffer(buffer);
+        ota_update("https://selfsigned.ota.tanmatsu.cloud/stable.bin", firmware_update_callback);
+    }
 }
