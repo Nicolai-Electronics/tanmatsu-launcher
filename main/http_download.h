71eaa8e2d894922b86972def179e1aa9b0bd173a
Renze Nicolai <renze@rnplus.nl>
2025-07-19 17:33:54 +0200

    Add file download utility

---
+++ main/http_download.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+bool download_file(const char* url, const char* path);
+bool download_ram(const char* url, uint8_t** ptr, size_t* size);
