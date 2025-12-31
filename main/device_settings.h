2a4ab30a8f91e630c2e6fc36857c393879d03af3
Renze Nicolai <renze@rnplus.nl>
2025-12-31 02:11:08 +0100

    Initial work on UI for chat functionality (#79)

--- main/device_settings.h
+++ main/device_settings.h
@@ -20,6 +20,14 @@ esp_err_t device_settings_get_repo_server(char* out_value, size_t max_length);
 esp_err_t device_settings_set_repo_server(const char* value);
 esp_err_t device_settings_get_repo_base_uri(char* out_value, size_t max_length);
 esp_err_t device_settings_set_repo_base_uri(const char* value);
-void device_settings_get_default_http_user_agent(char* out_value, size_t max_length);
+void      device_settings_get_default_http_user_agent(char* out_value, size_t max_length);
 esp_err_t device_settings_get_http_user_agent(char* out_value, size_t max_length);
 esp_err_t device_settings_set_http_user_agent(const char* value);
+
+// Owner settings
+esp_err_t device_settings_get_owner_nickname(char* out_value, size_t max_length);
+esp_err_t device_settings_set_owner_nickname(const char* value);
+esp_err_t device_settings_get_owner_birthday_day(uint8_t* out_day);
+esp_err_t device_settings_set_owner_birthday_day(uint8_t day);
+esp_err_t device_settings_get_owner_birthday_month(uint8_t* out_month);
+esp_err_t device_settings_set_owner_birthday_month(uint8_t month);
