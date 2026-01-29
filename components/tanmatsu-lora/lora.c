c3e4cbb03edad5f81eb304f07d79bcae92d06cf7
Renze Nicolai <renze@rnplus.nl>
2026-01-29 02:01:16 +0100

    Move LoRa protocol to in-tree component

--- components/tanmatsu-lora/lora.c
+++ components/tanmatsu-lora/lora.c
@@ -5,7 +5,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/idf_additions.h"
 #include "freertos/queue.h"
-#include "lora_protocol.h"
 #include "portmacro.h"
 
 #if defined(CONFIG_IDF_TARGET_ESP32P4)
@@ -13,18 +12,19 @@
 #endif
 
 #if defined(CONFIG_BSP_TARGET_TANMATSU) || defined(CONFIG_BSP_TARGET_KONSOOL)
-#include "tanmatsu_coprocessor.h"
 #include "bsp/tanmatsu.h"
+#include "tanmatsu_coprocessor.h"
 #endif
 
 static const char TAG[] = "lora";
 
-QueueHandle_t     lora_packet_queue          = NULL;
-SemaphoreHandle_t lora_mutex                 = NULL;
-SemaphoreHandle_t lora_transaction_semaphore = NULL;
-uint32_t          lora_sequence_number       = 0;
-uint8_t           lora_packet_buffer[sizeof(uint32_t) + 512];
-size_t            lora_packet_size = 0;
+QueueHandle_t                      lora_packet_queue          = NULL;
+SemaphoreHandle_t                  lora_mutex                 = NULL;
+SemaphoreHandle_t                  lora_transaction_semaphore = NULL;
+uint32_t                           lora_sequence_number       = 0;
+uint8_t                            lora_packet_buffer[sizeof(uint32_t) + 512];
+size_t                             lora_packet_size = 0;
+static lora_protocol_lora_packet_t lora_packet      = {0};
 
 static esp_err_t lora_transaction(const uint8_t* request, size_t request_length, uint8_t* out_response,
                                   size_t* response_length, size_t max_response_length) {
@@ -55,47 +55,48 @@ static esp_err_t lora_transaction(const uint8_t* request, size_t request_length,
 }
 
 esp_err_t lora_transaction_receive(uint8_t* packet, size_t length) {
+    if (!lora_mutex || !lora_transaction_semaphore || !lora_packet_queue) {
+        return ESP_ERR_INVALID_STATE;
+    }
     if (length > sizeof(lora_packet_buffer) || length < sizeof(lora_protocol_header_t)) {
         return ESP_ERR_INVALID_SIZE;
     }
     lora_protocol_header_t* header = (lora_protocol_header_t*)packet;
     if (header->type == LORA_PROTOCOL_TYPE_PACKET_RX) {
-        // to-do add to queue
-        printf("Received LoRa packet\r\n");
-        for (size_t i = sizeof(lora_protocol_header_t); i < length - sizeof(lora_protocol_header_t); i++) {
-            printf("%02X ", packet[i]);
-        }
-        printf("\r\n");
-#if defined(CONFIG_BSP_TARGET_TANMATSU) || defined(CONFIG_BSP_TARGET_KONSOOL)
-        tanmatsu_coprocessor_handle_t handle;
-        bsp_tanmatsu_coprocessor_get_handle(&handle);
-        tanmatsu_coprocessor_set_message(handle, true, false, false, true, false, false, false, false);
-        vTaskDelay(pdMS_TO_TICKS(500));
-        tanmatsu_coprocessor_set_message(handle, false, false, false, false, false, false, false, false);
-#endif
-        return ESP_OK;
+        // Received LoRa packet
+        lora_packet.length = length - sizeof(lora_protocol_header_t);
+        memcpy(lora_packet.data, packet + sizeof(lora_protocol_header_t), lora_packet.length);
+        xQueueSend(lora_packet_queue, &lora_packet, 0);
+    } else {
+        // Response to a transaction
+        memcpy(lora_packet_buffer, packet, length);
+        lora_packet_size = length;
+        xSemaphoreGive(lora_transaction_semaphore);
     }
-    // Else it's a response to a transaction
-    memcpy(lora_packet_buffer, packet, length);
-    lora_packet_size = length;
-    xSemaphoreGive(lora_transaction_semaphore);
     return ESP_OK;
 }
 
-esp_err_t lora_init(QueueHandle_t packet_queue) {
-    lora_packet_queue          = packet_queue;
+esp_err_t lora_init(uint32_t packet_queue_size) {
     lora_mutex                 = xSemaphoreCreateMutex();
     lora_transaction_semaphore = xSemaphoreCreateBinary();
-    if (lora_mutex == NULL || lora_transaction_semaphore == NULL) {
+    lora_packet_queue          = xQueueCreate(packet_queue_size, sizeof(lora_protocol_lora_packet_t));
+    if (lora_mutex == NULL || lora_transaction_semaphore == NULL || lora_packet_queue == NULL) {
         if (lora_mutex != NULL) {
             vSemaphoreDelete(lora_mutex);
         }
         if (lora_transaction_semaphore != NULL) {
             vSemaphoreDelete(lora_transaction_semaphore);
         }
+        if (lora_packet_queue != NULL) {
+            vQueueDelete(lora_packet_queue);
+        }
         return ESP_ERR_NO_MEM;
     }
     return ESP_OK;
+}
+
+QueueHandle_t lora_get_packet_queue(void) {
+    return lora_packet_queue;
 }
 
 esp_err_t lora_get_mode(lora_protocol_mode_t* out_mode) {
@@ -261,9 +262,35 @@ esp_err_t lora_get_status(lora_protocol_status_params_t* out_status) {
 }
 
 esp_err_t lora_send_packet(const lora_protocol_lora_packet_t* packet) {
-    return ESP_ERR_NOT_SUPPORTED;
+    size_t                  request_length = sizeof(lora_protocol_header_t) + packet->length;
+    uint8_t                 request[request_length];
+    lora_protocol_header_t* header = (lora_protocol_header_t*)request;
+    header->sequence_number        = lora_sequence_number;
+    header->type                   = LORA_PROTOCOL_TYPE_PACKET_TX;
+    uint8_t* data_ptr              = (uint8_t*)(request + sizeof(lora_protocol_header_t));
+    memcpy(data_ptr, packet->data, packet->length);
+    uint8_t   response[sizeof(lora_protocol_header_t)] = {0};
+    size_t    response_length                          = 0;
+    esp_err_t result = lora_transaction(request, request_length, response, &response_length, sizeof(response));
+    if (result != ESP_OK) {
+        return result;
+    }
+    if (response_length < sizeof(lora_protocol_header_t)) {
+        ESP_LOGE(TAG, "Invalid response length: %u\r\n", response_length);
+        return ESP_FAIL;
+    }
+    lora_protocol_header_t* response_header = (lora_protocol_header_t*)response;
+    if (response_header->sequence_number != header->sequence_number) {
+        ESP_LOGE(TAG, "Invalid response sequence number: %u\r\n", response_header->sequence_number);
+        return ESP_FAIL;
+    }
+    if (response_header->type != LORA_PROTOCOL_TYPE_ACK) {
+        ESP_LOGE(TAG, "Invalid response type: %u\r\n", response_header->type);
+        return ESP_FAIL;
+    }
+    return ESP_OK;
 }
 
-esp_err_t lora_receive_packet(uint8_t* out_buffer, uint8_t* out_length, uint16_t max_length) {
-    return ESP_ERR_NOT_SUPPORTED;
+esp_err_t lora_receive_packet(lora_protocol_lora_packet_t* out_packet, TickType_t timeout) {
+    return xQueueReceive(lora_packet_queue, out_packet, timeout) ? ESP_OK : ESP_FAIL;
 }
