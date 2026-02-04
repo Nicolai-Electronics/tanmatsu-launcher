3802c8a89ef4795b11a84119d11bcce99566b930
Renze Nicolai <renze@rnplus.nl>
2026-02-05 00:20:51 +0100

    Add LoRa device settings

--- main/device_settings.c
+++ main/device_settings.c
@@ -57,6 +57,88 @@ static esp_err_t device_settings_set_u8(const char* key, uint8_t value) {
     return res;
 }
 
+static esp_err_t device_settings_get_u16(const char* key, uint16_t default_value, uint16_t* out_value) {
+    if (key == NULL || out_value == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    nvs_handle_t nvs_handle;
+    esp_err_t    res = nvs_open(NVS_NAMESPACE, NVS_READONLY, &nvs_handle);
+    if (res != ESP_OK) {
+        *out_value = default_value;
+        return res;
+    }
+    uint16_t value;
+    res = nvs_get_u16(nvs_handle, key, &value);
+    if (res != ESP_OK) {
+        *out_value = default_value;
+        nvs_close(nvs_handle);
+        return res;
+    }
+    nvs_close(nvs_handle);
+
+    *out_value = value;
+    return res;
+}
+
+static esp_err_t device_settings_set_u16(const char* key, uint16_t value) {
+    if (key == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    nvs_handle_t nvs_handle;
+    esp_err_t    res = nvs_open(NVS_NAMESPACE, NVS_READWRITE, &nvs_handle);
+    if (res != ESP_OK) {
+        return res;
+    }
+    res = nvs_set_u16(nvs_handle, key, value);
+    if (res != ESP_OK) {
+        nvs_close(nvs_handle);
+        return res;
+    }
+    nvs_close(nvs_handle);
+    return res;
+}
+
+static esp_err_t device_settings_get_u32(const char* key, uint32_t default_value, uint32_t* out_value) {
+    if (key == NULL || out_value == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    nvs_handle_t nvs_handle;
+    esp_err_t    res = nvs_open(NVS_NAMESPACE, NVS_READONLY, &nvs_handle);
+    if (res != ESP_OK) {
+        *out_value = default_value;
+        return res;
+    }
+    uint32_t value;
+    res = nvs_get_u32(nvs_handle, key, &value);
+    if (res != ESP_OK) {
+        *out_value = default_value;
+        nvs_close(nvs_handle);
+        return res;
+    }
+    nvs_close(nvs_handle);
+
+    *out_value = value;
+    return res;
+}
+
+static esp_err_t device_settings_set_u32(const char* key, uint32_t value) {
+    if (key == NULL) {
+        return ESP_ERR_INVALID_ARG;
+    }
+    nvs_handle_t nvs_handle;
+    esp_err_t    res = nvs_open(NVS_NAMESPACE, NVS_READWRITE, &nvs_handle);
+    if (res != ESP_OK) {
+        return res;
+    }
+    res = nvs_set_u32(nvs_handle, key, value);
+    if (res != ESP_OK) {
+        nvs_close(nvs_handle);
+        return res;
+    }
+    nvs_close(nvs_handle);
+    return res;
+}
+
 static esp_err_t device_settings_get_percentage(const char* key, uint8_t default_value, uint8_t minimum_value,
                                                 uint8_t* out_percentage) {
     esp_err_t res = device_settings_get_u8(key, default_value, out_percentage);
@@ -235,9 +317,77 @@ esp_err_t device_settings_set_owner_birthday_day(uint8_t day) {
 }
 
 esp_err_t device_settings_get_owner_birthday_month(uint8_t* out_month) {
-    return device_settings_get_u8("owner.bday.month", 1, out_month);
+    return device_settings_get_u8("owner.bday.mon", 1, out_month);
 }
 
 esp_err_t device_settings_set_owner_birthday_month(uint8_t month) {
-    return device_settings_set_u8("owner.bday.month", month);
+    return device_settings_set_u8("owner.bday.mon", month);
+}
+
+// LoRa settings
+
+esp_err_t device_settings_get_lora_frequency(uint32_t* out_value) {
+    return device_settings_get_u32("lora.freq", 869618000, out_value);
+}
+
+esp_err_t device_settings_set_lora_frequency(uint32_t frequency) {
+    return device_settings_set_u32("lora.freq", frequency);
+}
+
+esp_err_t device_settings_get_lora_spreading_factor(uint8_t* out_sf) {
+    return device_settings_get_u8("lora.sf", 8, out_sf);
+}
+
+esp_err_t device_settings_set_lora_spreading_factor(uint8_t sf) {
+    return device_settings_set_u8("lora.sf", sf);
+}
+
+esp_err_t device_settings_get_lora_bandwidth(uint16_t* out_bandwidth) {
+    return device_settings_get_u16("lora.bandwidth", 62, out_bandwidth);
+}
+
+esp_err_t device_settings_set_lora_bandwidth(uint16_t bandwidth) {
+    return device_settings_set_u16("lora.bandwidth", bandwidth);
+}
+
+esp_err_t device_settings_get_lora_coding_rate(uint8_t* out_coding_rate) {
+    return device_settings_get_u8("lora.codingrate", 8, out_coding_rate);
+}
+
+esp_err_t device_settings_set_lora_coding_rate(uint8_t coding_rate) {
+    return device_settings_set_u8("lora.codingrate", coding_rate);
+}
+
+esp_err_t device_settings_get_lora_power(uint8_t* out_power) {
+    return device_settings_get_u8("lora.power", 22, out_power);
+}
+
+esp_err_t device_settings_set_lora_power(uint8_t power) {
+    return device_settings_set_u8("lora.power", power);
+}
+
+// Meshcore settings
+
+esp_err_t device_settings_get_meshcore_name(char* out_value, size_t max_length) {
+    return device_settings_get_string("mc.name", "Tanmatsu", out_value, max_length);
+}
+
+esp_err_t device_settings_set_meshcore_name(const char* value) {
+    return device_settings_set_string("mc.name", value);
+}
+
+esp_err_t device_settings_get_meshcore_private_key(char* out_value, size_t max_length) {
+    return device_settings_get_string("mc.private_key", "", out_value, max_length);
+}
+
+esp_err_t device_settings_set_meshcore_private_key(const char* value) {
+    return device_settings_set_string("mc.private_key", value);
+}
+
+esp_err_t device_settings_get_meshcore_public_key(char* out_value, size_t max_length) {
+    return device_settings_get_string("mc.public_key", "", out_value, max_length);
+}
+
+esp_err_t device_settings_set_meshcore_public_key(const char* value) {
+    return device_settings_set_string("mc.public_key", value);
 }
